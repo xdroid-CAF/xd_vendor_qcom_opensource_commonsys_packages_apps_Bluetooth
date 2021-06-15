10f7908154278a188e115befac047b76bfab9b20
janardhana rao bokka <jbokka@codeaurora.org>
2021-06-15 12:16:08 +0530

    GATT: Clearing all the registering Gatt ID during BT Turn OFF
    Clearing all the registering Gatt ID during BT Turn OFF
    1. Registered Scanners
    2, Registered Advertisers
    
    Change-Id: I7770a60dc93f8cf7e30579416c52dbe54a37f774

--- src/com/android/bluetooth/gatt/ScanManager.java
+++ src/com/android/bluetooth/gatt/ScanManager.java
@@ -248,6 +248,10 @@ public class ScanManager {
         sendMessage(MSG_STOP_BLE_SCAN, client);
     }
 
+    void cancelScans(ScanClient client) {
+      mHandler.handleStopScan(client);
+    }
+
     void flushBatchScanResults(ScanClient client) {
         sendMessage(MSG_FLUSH_BATCH_RESULTS, client);
     }
